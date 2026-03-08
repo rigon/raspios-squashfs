36cb8b9866ba052d3038a42675ba298e0e3260aa
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2026-03-08 20:37:57 +0000

    Path fix

--- chroot/run.sh
+++ chroot/run.sh
@@ -2,6 +2,8 @@
 
 /bin/bash
 
+cd /chroot/
 cat packages-remove  | sed '/^#/d; /^$/d' | xargs apt remove  -y
 cat packages-install | sed '/^#/d; /^$/d' | xargs apt install -y
 apt autoremove -y
+#apt update && apt upgrade
