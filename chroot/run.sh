70c5ed87b74b72d0ca9dbf37b012992d75f62ac5
Ricardo Gonçalves <ricar.goncalves@gmail.com>
2026-06-24 13:16:46 +0100

    Improve apt install/remove/upgrade in a single step

--- chroot/run.sh
+++ chroot/run.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+cd /chroot/
+
 /bin/bash
 
-apt update
+apt-get update
+
+# Add/remove the desired packages in the final build
+PACKAGES=$(cat packages | sed '/^#/d; /^$/d' | sort -u)
+INSTALLED=$(apt-mark showmanual | sort -u)
+readarray -t TO_INSTALL < <(comm -13 <(echo "$INSTALLED") <(echo "$PACKAGES"))
+readarray -t TO_REMOVE < <(comm -23 <(echo "$INSTALLED") <(echo "$PACKAGES"))
 
 # Add live-boot to allow booting from squashfs
 sed -i 's/^MODULES=dep/MODULES=most/' /etc/initramfs-tools/initramfs.conf
-apt-get install -y live-boot
+apt-get upgrade -y live-boot+ "${TO_INSTALL[@]/%/+}" "${TO_REMOVE[@]/%/-}"
 sed -i 's/^MODULES=most/MODULES=dep/' /etc/initramfs-tools/initramfs.conf
 
-cd /chroot/
-cat packages-remove  | sed '/^#/d; /^$/d' | xargs apt remove  -y
-cat packages-install | sed '/^#/d; /^$/d' | xargs apt install -y
-#apt update && apt upgrade
-apt-get clean && apt-get autoremove --purge -y
+apt-get autoremove --purge -y
+# apt-get upgrade -y
+
+/bin/bash
+
+apt clean
 rm -rf /var/lib/apt/lists/*
 rm /boot/initrd.img-* /boot/vmlinuz-*
 
@@ -24,4 +33,3 @@ proc            /proc           proc    defaults  0 0
 tmpfs           /tmp            tmpfs   defaults  0 0
 EOF
 
-/bin/bash
